c6f7059c87450a2ca26ae40adc9531f484eb28d7
Ankur Rastogi <ankur.rastogi01@nagarro.com>
2021-03-17 12:08:25 +0530

    built in generics

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,13 @@
-const names = ["Ankur", "Golu"];
+// 1. Default type - Array
+const names: Array<string> = ["Ankur", "Golu"]; //string[] --both are same
+// 2. Default type - Promise
+const promise: Promise<string> = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("This is done!");
+    reject("Error!");
+  }, 2000);
+});
+
+promise.then((data: string) => {
+  console.log(data);
+});
