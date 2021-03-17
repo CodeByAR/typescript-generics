eb7d4c38c996a2449aa692ec49ab3f88e712a763
Ankur Rastogi <ankur.rastogi01@nagarro.com>
2021-03-17 12:09:29 +0530

    type constraints

--- src/app.ts
+++ src/app.ts
@@ -11,3 +11,14 @@ const promise: Promise<string> = new Promise((resolve, reject) => {
 promise.then((data: string) => {
   console.log(data);
 });
+
+//extends implements type constraints
+function merge<T extends object, U extends object>(objA: T, objB: U) {
+  return Object.assign(objA, objB);
+}
+
+const mergeObj = merge({ name: "Ank", hobbies: ["coding"] }, { age: 26 });
+
+//will give error because number is not an object
+//const mergeObj1 = merge({ name: "Ank", hobbies: ["coding"] }, 30);
+console.log(mergeObj);
