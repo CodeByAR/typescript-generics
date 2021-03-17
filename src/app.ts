735e4ad562c117aa3a12c4d4f27e6c529028183c
Ankur Rastogi <ankur.rastogi01@nagarro.com>
2021-03-17 12:37:48 +0530

    Generic Utilities & Generic vs Union

--- src/app.ts
+++ src/app.ts
@@ -51,36 +51,86 @@ console.log(extractAndConvert({ name: "ank" }, "name"));
 //throws error when key doesn't exist on object passed
 //console.log(extractAndConvert({name: 'ank'}, 'name1'));
 
-
 //Generic Class
 class DataStorage<T extends string | boolean | number> {
-    private data: T[] = [];
-  
-    addItem(item: T) {
-      this.data.push(item);
-    }
-  
-    removeItem(item: T) {
-      this.data.splice(this.data.indexOf(item), 1);
-    }
-  
-    getItems(): T[] {
-      return this.data;
-    }
+  private data: T[] = [];
+
+  addItem(item: T) {
+    this.data.push(item);
   }
-  
-  const txtStorage = new DataStorage<string>();
-  txtStorage.addItem("Ank");
-  txtStorage.addItem("Gol");
-  //throws error - string cant store number
-  //txtStorage.addItem(12);
-  txtStorage.removeItem("Gol");
-  console.log(txtStorage.getItems());
-  
-  const numStorage = new DataStorage<number>();
-  numStorage.addItem(21);
-  numStorage.addItem(12);
-  //throws error - number cant store string
-  //numStorage.addItem('Gol');
-  numStorage.removeItem(12);
-  console.log(numStorage.getItems());
+
+  removeItem(item: T) {
+    this.data.splice(this.data.indexOf(item), 1);
+  }
+
+  getItems(): T[] {
+    return this.data;
+  }
+}
+
+const txtStorage = new DataStorage<string>();
+txtStorage.addItem("Ank");
+txtStorage.addItem("Gol");
+//throws error - string cant store number
+//txtStorage.addItem(12);
+txtStorage.removeItem("Gol");
+console.log(txtStorage.getItems());
+
+const numStorage = new DataStorage<number>();
+numStorage.addItem(21);
+numStorage.addItem(12);
+//throws error - number cant store string
+//numStorage.addItem('Gol');
+numStorage.removeItem(12);
+console.log(numStorage.getItems());
+
+//Generics Utilities - Provided by TypeScript for controlling the behavior
+interface CourseGoal {
+  title: string;
+  description: string;
+  completeUntil: Date;
+}
+
+function createCourseGoal(
+  title: string,
+  description: string,
+  completeUntil: Date
+): CourseGoal {
+  //Use Partial when the object property assignment is to be done lazily
+  let courseGoal: Partial<CourseGoal> = {};
+  courseGoal.title = title;
+  courseGoal.description = description;
+  courseGoal.completeUntil = completeUntil;
+  return courseGoal as CourseGoal;
+}
+
+//Use Readonly when the data is to be frozen
+const namesArr: Readonly<string[]> = ["Ank", "Gol"];
+//Push and Pop method gives error
+//namesArr.push('Gol 1');
+//namesArr.pop();
+
+//Generic vs Union
+class DataStorageUnion {
+  private data: (string | number | boolean)[] = [];
+
+  addItem(item: string | number | boolean) {
+    this.data.push(item);
+  }
+
+  removeItem(item: string | number | boolean) {
+    this.data.splice(this.data.indexOf(item), 1);
+  }
+
+  getItems(): (string | number | boolean)[] {
+    return this.data;
+  }
+}
+
+//Generics confines the type to single whereas Union allows to use mixture of types to be used
+const unionStorage = new DataStorageUnion();
+unionStorage.addItem("Ank");
+unionStorage.addItem(true);
+unionStorage.addItem(12);
+unionStorage.removeItem(true);
+console.log(unionStorage.getItems());
